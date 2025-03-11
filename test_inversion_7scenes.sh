13e6c27287d68d6a7f29bd52581a26feddc795da
PHANTOM0122 <wilko97@khu.ac.kr>
2025-03-11 19:18:40 +0900

    Add test inversion

---
+++ test_inversion_7scenes.sh
@@ -0,0 +1,7 @@
+python3 main.py -d chess -e False -r True -t True -i True -g cuda:0
+python3 main.py -d heads -e False -r True -t True -i True -g cuda:0
+python3 main.py -d fire -e False -r True -t True -i True -g cuda:0
+python3 main.py -d office -e False -r True -t True -i True -g cuda:0
+python3 main.py -d redkitchen -e False -r True -t True -i True -g cuda:0
+python3 main.py -d pumpkin -e False -r True -t True -i True -g cuda:0
+python3 main.py -d stairs -e False -r True -t True -i True -g cuda:0
